4419e5bc963c0d30c9cbf99967d339fdb8894aa5
Makoto <makoto@emurasoft.com>
2021-06-25 12:16:12 -0700

    react-leaflet

--- index.tsx
+++ index.tsx
@@ -1,8 +1,20 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
 
 function App(): React.ReactElement {
-	return <p>Hello world!</p>
+	return <MapContainer center={[47.5843038,-122.1516135]} zoom={14} style={{height: '600px', width: '600px'}}>
+		<TileLayer
+			attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+			url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+			detectRetina ={true}
+		/>
+		<Marker position={[47.5843038,-122.1516135]}>
+			<Popup>
+				A pretty CSS3 popup. <br /> Easily customizable.
+			</Popup>
+		</Marker>
+	</MapContainer>;
 }
 
 ReactDOM.render(
