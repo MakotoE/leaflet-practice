76841797ed3df46016e513e9d9626a6f157bdb8f
Makoto <makoto@emurasoft.com>
2021-06-28 10:59:23 -0700

    copy icons

--- webpack.js
+++ webpack.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     mode: 'development',
@@ -37,5 +38,10 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({template: 'index.html'}),
+        new CopyPlugin({
+            patterns: [
+                { from: "icons", to: "icons" },
+            ],
+        }),
     ],
 };
